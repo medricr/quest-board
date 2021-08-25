2f5a4df98709b3c02cdf6ef858fb3c6a013f7b6d
medricr <medricr@gmail.com>
2021-08-24 21:28:53 -0700

    added comments

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
+// bring in the twitter package
 const Twitter = require('twitter');
+// bring in the file with credentials
 const auth = require('./auth.json');
-
+// initialize clinet as instance of Twitter object
 const client = new Twitter({
   consumer_key: auth.api_key,
   consumer_secret: auth.api_secret_key,
@@ -8,6 +10,7 @@ const client = new Twitter({
   access_token_secret: auth.access_token_secret
 });
 
+// post tweet with catch block
 client.post('statuses/update', {status: 'Adventure awaits!'})
   .then(function(tweet){
     console.log(tweet)
@@ -15,4 +18,3 @@ client.post('statuses/update', {status: 'Adventure awaits!'})
   .catch(function(error){
     console.log(error)
   })
-
